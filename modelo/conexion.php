c300a6b9449259270b52cce77fce5ac53d0f50e0
AlexisHuaman <78916588+AlexisHuaman@users.noreply.github.com>
2024-10-21 00:00:05 -0500

    version30

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,12 +1,12 @@
 <?php
 class conexion
 {
-    private $servidor = "localhost";
-    private $db = "billetera";
+    private $servidor = "sql202.infinityfree.com";
+    private $db = "if0_37553523_db_billetera";
     private $puerto = "3306";
     private $charset = "UTF8";
-    private $usuario = "root";
-    private $contrasena = "";
+    private $usuario = "if0_37553523";
+    private $contrasena = "ZrU7DvQnhyeJ6sw";
     public $pdo = null;
     private $atributos = [PDO::ATTR_CASE => CASE_LOWER, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
 
